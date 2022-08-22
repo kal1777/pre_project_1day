96f809963c5d845a7b3b77871c15843c120f1cd8
kal1777 <kimnayul17@gmail.com>
2022-08-22 19:35:22 +0900

    initial

---
+++ src/main/java/com/example/pre_project_1day/PreProject1dayApplication.java
@@ -0,0 +1,13 @@
+package com.example.pre_project_1day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProject1dayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreProject1dayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pre_project_1day/PreProject1dayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pre_project_1day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProject1dayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
